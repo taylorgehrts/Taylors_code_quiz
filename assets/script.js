1c1bb33779f7f753038a9a2a2674f75877f3a66a
taylorgehrts <taylorgehrts@gmail.com>
2023-05-18 14:50:07 -0700

    added start section to html restructured ids and classes, added basic js to have button press hide and display sections

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-console.log(this);
+const startBtn = document.getElementById("startbut");
+const startHide = document.getElementById("startHide")
+const questHide = document.getElementById("questionHide")
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    startHide.classList.add("hide")
+    questHide.classList.remove("hide")
+
+}
+
+function selectAnswer() {
+
+}

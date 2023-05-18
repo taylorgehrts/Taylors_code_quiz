4be61df707e79e8ce8e48dfb8c9103dfacf188ff
taylorgehrts <taylorgehrts@gmail.com>
2023-05-18 16:38:57 -0700

    added timer function, call it in the start function, handles display hide

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,49 @@
+//Button Elements
 const startBtn = document.getElementById("startbut");
-const but1 =document.getElementById("but1");
+const but1 = document.getElementById("but1");
+//Document elements
+const startHide = document.getElementById("startHide");
+const questHide = document.getElementById("questionHide");
+const finishHide = document.getElementById("finishHide");
+var timeLeft = document.querySelector(".timeLeft");
 
-const startHide = document.getElementById("startHide")
-const questHide = document.getElementById("questionHide")
-const finishHide = document.getElementById("finishHide")
-
+//Event listeners
 startBtn.addEventListener("click", startQuiz);
-but1.addEventListener("click", selectAnswer)
+but1.addEventListener("click", selectAnswer);
 
+//global variables
+var secondsLeft = 10;
+
+
+//start quiz function
 function startQuiz() {
-    startHide.classList.add("hide")
-    questHide.classList.remove("hide")
+    startHide.classList.add("hide");
+    questHide.classList.remove("hide");
+    countDown();
 
 }
-
+//answer function
 function selectAnswer() {
-    questHide.classList.add("hide")
-    finishHide.classList.remove("hide")
+
 
 }
+
+//timer function
+function countDown() {
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timeInterval);
+            questHide.classList.add("hide");
+            finishHide.classList.remove("hide");
+        }
+    },
+        1000);
+
+}
+
+
+
+

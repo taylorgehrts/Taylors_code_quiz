303bca5e3b68540dc5631decc978ebe3b0a0edea
taylorgehrts <taylorgehrts@gmail.com>
2023-05-18 15:25:43 -0700

    added finish section with form, styled that, added placeholder function to hide and display

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 const startBtn = document.getElementById("startbut");
+const but1 =document.getElementById("but1");
+
 const startHide = document.getElementById("startHide")
 const questHide = document.getElementById("questionHide")
+const finishHide = document.getElementById("finishHide")
+
 startBtn.addEventListener("click", startQuiz);
+but1.addEventListener("click", selectAnswer)
 
 function startQuiz() {
     startHide.classList.add("hide")
@@ -10,5 +15,7 @@ function startQuiz() {
 }
 
 function selectAnswer() {
+    questHide.classList.add("hide")
+    finishHide.classList.remove("hide")
 
 }
